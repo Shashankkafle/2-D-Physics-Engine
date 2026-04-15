072391ca21483ef969402b867bac8dc8e32418dd
Shashank Kafle <shashank.kaphle@gmail.com>
2026-04-15 21:02:58 +0700

    allow animation control

--- main.js
+++ main.js
@@ -1,84 +1,102 @@
 function timeout(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function setObjectPosition(object, position) {
-	console.log('inside relocate', object, position);
-	object.style.top = position.y + 'px';
-	object.style.left = position.x + 'px';
+    object.style.top = position.y + 'px';
+    object.style.left = position.x + 'px';
 }
+
 function createCircle(radious, color = 'black', position = { x: 0, y: 550 }) {
-	const newCircle = document.createElement('div');
-	newCircle.style.cssText = `  width: ${radious}px;
+    const newCircle = document.createElement('div');
+    newCircle.style.cssText = `  width: ${radious}px;
   height: ${radious}px;
   background-color: ${color};
   border-radius: 25px;
   position: absolute;
   top: ${position.y}px;
   left: ${position.x}px;`;
-	document.getElementById('animationArea').appendChild(newCircle);
-	return newCircle;
+    document.getElementById('animationArea').appendChild(newCircle);
+    return newCircle;
 }
 
-async function boxBounce(object, initialPosition = { x: 0, y: 550 },delay=1) {
-	let x = initialPosition.x,
-		y = initialPosition.y;
-	let xDir = 5;
-	let yDir = 7;
-	while (1) {
-		await timeout(delay);
-		if (x > 1000 - 50 || x < 0) {
-			xDir = xDir * -1;
-		}
-		if (y > 600 - 50 || y < 0) {
-			yDir = yDir * -1;
-		}
-		x += xDir;
-		y += yDir;
-		// break
-		setObjectPosition(object, { x, y });
-	}
+async function boxBounce(object, initialPosition = { x: 0, y: 550 }, delay = 1) {
+    let x = initialPosition.x,
+        y = initialPosition.y;
+    let xDir = 5;
+    let yDir = 7;
+    while (1) {
+        await timeout(delay);
+        if (x > 1000 - 50 || x < 0) {
+            xDir = xDir * -1;
+        }
+        if (y > 600 - 50 || y < 0) {
+            yDir = yDir * -1;
+        }
+        x += xDir;
+        y += yDir;
+        setObjectPosition(object, { x, y });
+    }
 }
+
 async function projectile(
-	object,
-	angle,
-	force,
-	initialPosition = { x: 0, y: 550 }
+    object,
+    angle,
+    force,
+    initialPosition = { x: 0, y: 550 }
 ) {
-	let radians = (angle * Math.PI) / 180;
+    let radians = (angle * Math.PI) / 180;
 
-	let i = 0,
-		xForce = force * Math.cos(radians),
-		yForce = force * Math.cos(radians);
-	let x = initialPosition.x,
-		y = initialPosition.y;
-	while (y >= 0 && x <= 950 && y <= 550 && x >= 0) {
-		await timeout(100);
-		createCircle(6, 'red', { x, y });
-		setObjectPosition(object, { x, y });
-		i += 0.5;
-		y -= yForce * i - 0.5 * 10 * i * i;
-		x += xForce * i;
-		// if (i > 500) break;
-	}
+    let i = 0,
+        xForce = force * Math.cos(radians),
+        yForce = force * Math.sin(radians); 
+    let x = initialPosition.x,
+        y = initialPosition.y;
+    while (1) {
+        await timeout(100);
+        createCircle(6, 'red', { x, y });
+        setObjectPosition(object, { x, y });
+        i += 0.1;
+        y -= yForce * i - 0.5 * 10 * i * i;
+        x += xForce * i;
+		if(y < 0 || x > 950 || y > 550 || x < 0){
+			object.remove();
+			break;
+		}
+    }
 }
 
-async function plot(eqn, start, end,resolution) {
-	let x = start;
-	while (x <= end) {
-		const point = createCircle(3, "black");
-		setObjectPosition(point, { x:x*50, y: eqn(x)*50 });
-		x+=resolution
-	}
+async function plot(eqn, start, end, resolution) {
+    let x = start;
+    while (x <= end) {
+        const point = createCircle(3, "black");
+        setObjectPosition(point, { x: x * 50, y: eqn(x) * 50 });
+        x += resolution
+    }
 }
-plot(Math.sin,0,7.14,0.1)
 
-// const circle1 = createCircle(50, 'black');
-// const circle2 = createCircle(50, 'red');
-// // const circle3 = createCircle(50, 'blue');
-// // const circle4 = createCircle(50, 'green');
-// // boxBounce(circle1);
-// // boxBounce(circle2,{x:222,y:333});
-// // boxBounce(circle3,{x:150,y:12});
-// // boxBounce(circle4,{x:750,y:150});
-// projectile(circle2, 15, 23);
+
+function runBounce() {
+  const delay = Number(document.getElementById('bounceDelay').value);
+  const circle = createCircle(50, 'black');
+  boxBounce(circle, { x: 0, y: 550 }, delay); 
+}
+
+function runProjectile() {
+  const angle = Number(document.getElementById('projAngle').value);
+  const force = Number(document.getElementById('projForce').value);
+  const circle = createCircle(50, 'red');
+  projectile(circle, angle, force);
+}
+
+function runPlot() {
+  const funcName = document.getElementById('plotFunc').value;
+  const start = Number(document.getElementById('plotStart').value);
+  const end = Number(document.getElementById('plotEnd').value);
+  const res = Number(document.getElementById('plotRes').value);
+  plot(Math[funcName], start, end, res);
+}
+
+function clearScreen() {
+  document.getElementById('animationArea').innerHTML = '';
+}
